dc871858023c9072809f48c267214f050548ba92
mahadbajwa97 <mahadbajwa97@gmail.com>
2022-06-09 05:57:37 +0500

    App update

---
+++ encryptKey.js
@@ -0,0 +1,13 @@
+const ethers = require('ethers');
+const fs = require('fs');
+require("dotenv").config();
+
+async function main(){
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+const encryptedJsonKey = await wallet.encrypt(process.env.PRIVATE_KEY_PASSWORD
+    ,process.env.PRIVATE_KEY);
+fs.writeFile('./.encryptedKey.json', encryptedJsonKey)
+    console.log(encryptedJsonKey);
+}
+main();
+
